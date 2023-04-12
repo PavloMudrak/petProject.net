3c665589473df8efdff5df0124ba5e99f018b178
PavloMudrak <pavlo220697@gmail.com>
2023-04-13 02:07:47 +0300

    init comit

---
+++ DataAccessLayer/DataProviders/ICustomerDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DataProviders
+{
+    public interface ICustomerDataProvider
+    {
+        Task<List<Customer>> GetAllCustomersAsync();
+        Task<Customer> GetCustomerByIdAsync(int id);
+        Task AddCustomerAsync(Customer customer);
+        Task UpdateCustomerAsync(Customer customer);
+        Task DeleteCustomerAsync(int id);
+    }
+}
---
+++ ServerDevelopment/Data/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServerDevelopment.Data
+{
+    public interface ICustomerService
+    {
+        Task CreateAsync(Customer customer);
+        Task<Customer> GetByIdAsync(int id);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(int id);
+        Task<List<Customer>> GetAllCustomersAsync();
+    }
+}
